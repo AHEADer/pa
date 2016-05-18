d630c7d512b41d93315014afdbdff1e1d6adc063
tracer-ics2015 <tracer@njuics.org>
2016-05-18 17:33:24 +0800

    >  run 141220000 david Linux Aheader 4.5.1-1-ARCH #1 SMP PREEMPT Thu Apr 14 19:19:32 CEST 2016 x86_64 GNU/Linux  17:33:24 up  3:26,  6 users,  load average: 0.03, 0.13, 0.25 f542d3dcc0c02af2ce5d7a7b7cb78539272cb945

--- nemu/src/cpu/exec/jmp/call-template.h
+++ nemu/src/cpu/exec/jmp/call-template.h
@@ -4,18 +4,9 @@
 
 static void do_execute ()
 {
-    //cpu.esp -= DATA_BYTE;
-	//MEM_W(cpu.esp, cpu.eip+4);
-	//cpu.eip += op_src->val;
-
-	if (cpu.eflags.ZF)
-    {
-        int32_t tsrc = (DATA_TYPE_S)op_src->val;
-        cpu.eip += tsrc;
-//#if DATA_BYTE == 2
-//        cpu.eip &= 0x0000ffff;
-//#endif
-    }
+    cpu.esp -= DATA_BYTE;
+	MEM_W(cpu.esp, cpu.eip+4);
+	cpu.eip += op_src->val;
     print_asm_template1();
 }
 
