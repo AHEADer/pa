6572288237f6f5276eadff74e2bf9f3e79782c8c
tracer-ics2015 <tracer@njuics.org>
2016-06-27 16:53:57 +0800

    >  run 141220000 david Linux Aheader 4.6.2-1-ARCH #1 SMP PREEMPT Wed Jun 8 08:40:59 CEST 2016 x86_64 GNU/Linux  16:53:57 up  2:27,  4 users,  load average: 1.13, 0.47, 0.23 efc7c7d574cdc1f828bd08f3c0e8d89476c47db

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -98,7 +98,7 @@ static bool make_token(char *e) {
 		}
 
 		if(i == NR_REGEX) {
-			printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+			printf("1st no match at position %d\n%s\n%*.s^\n", position, e, position, "");
 			return false;
 		}
 	}
