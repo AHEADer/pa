cdcc941e8dc3068f721151305453aaaa92bbcef2
tracer-ics2015 <tracer@njuics.org>
2016-06-30 00:34:48 +0800

    >  run 141220000 david Linux Aheader 4.6.2-1-ARCH #1 SMP PREEMPT Wed Jun 8 08:40:59 CEST 2016 x86_64 GNU/Linux  00:34:48 up  2:20,  5 users,  load average: 0.27, 0.29, 0.31 fd3d9116caaf6db79544142cbebd2a9d97d9bbf3

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -160,7 +160,7 @@ static int cmd_p(char *args)
     }
 	//char *nstr = strtok(args, " ");
     printf("nr_symtab_entry is %d\n",nr_symtab_entry );
-    printf("strtab is %s\n",strtab );
+    printf("strtab is %c\n",strtab[9] );
     printf("symtab is %d\n",(int)symtab[9].st_name);
 	return 0;
 }
