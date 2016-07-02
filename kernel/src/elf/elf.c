57ab504d46ae1cfaabb378abc7e48a6bb4339a6f
tracer-ics2015 <tracer@njuics.org>
2016-07-02 09:05:26 +0800

    >  compile kernel 141220000 david Linux Aheader 4.6.2-1-ARCH #1 SMP PREEMPT Wed Jun 8 08:40:59 CEST 2016 x86_64 GNU/Linux  09:05:25 up 42 min,  3 users,  load average: 1.24, 0.67, 0.42 a31c204868e17bfd30d14850f763aaaf6d81128e

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,20 +31,24 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x464c457f;
+	const uint32_t elf_magic = 0xBadC0de;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	//panic("please implement me");
-        ph = (void *) ((char *)buf + elf->e_phoff);
-        int i;
-	for(i=0; i< elf->e_phnum; ++i) {
+	panic("please implement me");
+	for(; true; ) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
-                    uint8_t *mem = (void *) mm_malloc(ph->p_vaddr, ph->p_memsz);
-                    ramdisk_read(mem, ph->p_offset, ph->p_filesz);
-                    memset(mem+ph->p_filesz, 0, ph->p_memsz-ph->p_filesz);
+
+			/* TODO: read the content of the segment from the ELF file 
+			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
+			 */
+			 
+			 
+			/* TODO: zero the memory region 
+			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
+			 */
 
 
 #ifdef IA32_PAGE
@@ -54,7 +58,6 @@ uint32_t loader() {
 			if(brk < new_brk) { brk = new_brk; }
 #endif
 		}
-		++ph;
 	}
 
 	volatile uint32_t entry = elf->e_entry;
