076deacc499b53095a9f62811584e099f79649b9
tracer-ics2015 <tracer@njuics.org>
2016-07-02 09:29:27 +0800

    >  compile kernel 141220000 david Linux Aheader 4.6.2-1-ARCH #1 SMP PREEMPT Wed Jun 8 08:40:59 CEST 2016 x86_64 GNU/Linux  09:29:27 up  1:06,  5 users,  load average: 0.40, 0.52, 0.54 2eba78456c2be68e147c50fa145d456f89286d86

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,16 +31,18 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+	//panic("please implement me");
+	ph = (void *) ((char *)buf + elf->e_phoff);
+	int i;
+	for(i=0;i < elf->e_phnum;i++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
-
+			panic("the vaddr value is %d", ph->p_vaddr);
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
