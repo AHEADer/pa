d0415ff5acdcb7f34998b8c456d23732b7bca686
tracer-ics2015 <tracer@njuics.org>
2016-07-05 16:49:51 +0800

    >  compile NEMU 141220000 david Linux Aheader 4.6.2-1-ARCH #1 SMP PREEMPT Wed Jun 8 08:40:59 CEST 2016 x86_64 GNU/Linux  16:49:51 up  3:14,  5 users,  load average: 0.00, 0.07, 0.13 a48ce1db7fdc0d3e6755ff9b8d6ea09bc8c49c1

--- nemu/include/cpu/cache/L1_cache.h
+++ nemu/include/cpu/cache/L1_cache.h
@@ -9,13 +9,14 @@
 #define CACHE_OFFSET 6
 #define CACHE_NUM 7
 #define CACHE_TAG (32 - CACHE_NUM - CACHE_OFFSET)
+#define CACHE_MASK 0x3fL
 
 typedef union
 {
     struct
     {
         uint32_t tag:CACHE_TAG;
-        uint32_t num:CACHE_G_NUM;
+        uint32_t g_num:CACHE_G_NUM;
         uint32_t off:CACHE_OFFSET;
     };
     uint32_t addr;
@@ -23,15 +24,15 @@ typedef union
 
 typedef struct {
 	uint8_t content[CACHE_BLOCK_SIZE];
+	uint32_t tag:CACHE_TAG;
 	bool valid;
 } CACHE_BLOCK;
 
 CACHE_BLOCK cache1_buf[1<<CACHE_G_NUM][CACHE_WAYS];
-//TODO: 8-way set associative
-	
 
 void init_L1_cache();
-//void L1_cache_read(hwaddr_t addr, size_t len);
+uint32_t L1_cache_read(hwaddr_t addr, size_t len);
+void l1_cache_raddress(cache_addr_l1 l1_addr, uint8_t* data);
 //void L1_cache_write(hwaddr_t addr, size_t len);
 
 #endif
--- nemu/src/cpu/cache/L1_cache.c
+++ nemu/src/cpu/cache/L1_cache.c
@@ -1,4 +1,10 @@
 #include "cpu/cache/L1_cache.h"
+#include "misc.h"
+
+#define HW_MEM_SIZE (1<<27)
+
+uint32_t dram_read(hwaddr_t, size_t);
+void dram_write(hwaddr_t, size_t, uint32_t);
 
 void init_L1_cache(){
 	int i;
@@ -10,4 +16,61 @@ void init_L1_cache(){
 			cache1_buf[i][j].valid = false;
 		}
 	}
+}
+
+uint32_t L1_cache_read(hwaddr_t addr, size_t len){
+	//Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", addr);
+
+	uint32_t offset = addr & CACHE_MASK;
+	uint8_t temp[2 * CACHE_BLOCK_SIZE];
+	cache_addr_l1 l1_addr;
+	l1_addr.addr = addr;
+
+	l1_cache_raddress(l1_addr, temp);
+
+	if(offset + len > CACHE_BLOCK_SIZE) {
+		/* data cross the cache size boundary*/
+		l1_cache_raddress(l1_addr, temp + CACHE_BLOCK_SIZE);
+	}
+	return unalign_rw(temp + offset, 4);
+}
+
+void l1_cache_raddress(cache_addr_l1 l1_addr, uint8_t* data){
+	Assert(l1_addr.addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", l1_addr.addr);
+
+	static int rand = 0;
+	//uint32_t tag = l1_addr.tag;
+	uint32_t g_num = l1_addr.g_num;
+
+	int i, valid_num = -1;
+	for (i = 0; i < CACHE_WAYS; ++i)
+	{
+		if (cache1_buf[g_num][i].valid)
+		{
+			if (l1_addr.tag == cache1_buf[g_num][i].tag)
+			{
+				printf("hit cache\n");
+				break;
+			}
+		}
+		else
+			valid_num = i;
+	}
+	if (i!=CACHE_WAYS)
+	{
+		memcpy(data, cache1_buf[g_num][i].content, CACHE_BLOCK_SIZE);
+	}
+	else
+	{
+		if (valid_num < 0)
+		{
+			valid_num = rand;
+			rand = (rand + 1) % (CACHE_WAYS);
+		}
+		cache1_buf[g_num][valid_num].valid = true;
+		cache1_buf[g_num][valid_num].tag = l1_addr.tag;
+		//memcpy(cache1_buf[g_num][i].content, dram_read(l1_addr.addr, 0),CACHE_BLOCK_SIZE); read data from dram
+		memcpy(data, cache1_buf[g_num][i].content, CACHE_BLOCK_SIZE);
+	}
+
 }
