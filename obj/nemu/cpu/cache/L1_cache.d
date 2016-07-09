a079dc517d0d206f648131a752c636337c862733
AHEADer <dqawveird@gmail.com>
2016-07-09 23:27:59 +0800

    read bugs

--- obj/nemu/cpu/cache/L1_cache.d
+++
@@ -1,4 +0,0 @@
-obj/nemu/cpu/cache/L1_cache.o: nemu/src/cpu/cache/L1_cache.c \
- nemu/include/cpu/cache/L1_cache.h nemu/include/common.h \
- nemu/include/debug.h nemu/include/macro.h nemu/include/misc.h \
- nemu/include/common.h nemu/include/cpu/cache/L2_cache.h
--- obj/nemu/cpu/cache/L2_cache.d
+++
@@ -1,4 +0,0 @@
-obj/nemu/cpu/cache/L2_cache.o: nemu/src/cpu/cache/L2_cache.c \
- nemu/include/common.h nemu/include/debug.h nemu/include/macro.h \
- nemu/include/cpu/cache/L2_cache.h nemu/include/misc.h \
- nemu/include/common.h
